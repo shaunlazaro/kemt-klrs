7af02e950f9bc87341a110c98c596390b81e23ea
shaunlazaro <shaunlazaro@gmail.com>
2024-12-02 23:24:54 -0500

    try a different way to model pose

--- api/poseapp/admin.py
+++ api/poseapp/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import Pose, Routine
-from .constants import LANDMARK_CONNECTIONS
-import json
 
 # Customizing Pose admin view
 @admin.register(Pose)
@@ -16,62 +13,6 @@ class PoseAdmin(admin.ModelAdmin):
         # Assuming landmarks is a JSON field, this counts the number of landmarks
         return len(obj.landmarks) if obj.landmarks else 0
     pose_landmarks_count.short_description = 'Number of Landmarks'
-
-    def render_pose(self, obj):
-        """Renders the pose landmarks and connections as an HTML canvas."""
-        landmarks = obj.landmarks
-        if not landmarks:
-            return "No landmarks available"
-
-        # Prepare connections data for JavaScript
-        connections_json = json.dumps(LANDMARK_CONNECTIONS)
-        landmarks_json = json.dumps(landmarks)
-
-        html = f"""
-        <div>
-            <canvas id="poseCanvas-{obj.id}" width="300" height="300" 
-                style="border:1px solid black;"></canvas>
-            <script>
-                (function() {{
-                    const canvas = document.getElementById("poseCanvas-{obj.id}");
-                    const ctx = canvas.getContext("2d");
-                    const landmarks = {landmarks_json};
-                    const connections = {connections_json};
-
-                    // Scale factor for rendering
-                    const canvasWidth = canvas.width;
-                    const canvasHeight = canvas.height;
-
-                    // Draw connections
-                    ctx.strokeStyle = "blue";
-                    ctx.lineWidth = 2;
-                    connections.forEach(([startIdx, endIdx]) => {{
-                        const start = landmarks[startIdx];
-                        const end = landmarks[endIdx];
-                        if (start && end) {{
-                            ctx.beginPath();
-                            ctx.moveTo(start.x * canvasWidth, start.y * canvasHeight);
-                            ctx.lineTo(end.x * canvasWidth, end.y * canvasHeight);
-                            ctx.stroke();
-                        }}
-                    }});
-
-                    // Draw landmarks
-                    ctx.fillStyle = "red";
-                    landmarks.forEach(l => {{
-                        const x = l.x * canvasWidth;
-                        const y = l.y * canvasHeight;
-                        ctx.beginPath();
-                        ctx.arc(x, y, 3, 0, 2 * Math.PI);
-                        ctx.fill();
-                    }});
-                }})();
-            </script>
-        </div>
-        """
-        return format_html(html)
-
-    render_pose.short_description = "Pose Visual"
 
 # Customizing Routine admin view
 @admin.register(Routine)
--- api/poseapp/models.py
+++ api/poseapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 class Pose(models.Model):
     # Store the entire list of landmarks as a JSON field
@@ -7,7 +8,54 @@ class Pose(models.Model):
 
     def __str__(self):
         return f"Pose {self.id} with {len(self.landmarks)} landmarks"
+    
+    def render_pose(self):
+        # Generate JavaScript to render the pose
+        return mark_safe(f"""
+        <div>
+            <canvas id="poseCanvas-{self.id}" width="500" height="500" style="border:1px solid #000;"></canvas>
+            <script>
+                (function() {{
+                    const canvas = document.getElementById('poseCanvas-{self.id}');
+                    const ctx = canvas.getContext('2d');
+                    const landmarks = {self.landmarks};
 
+                    // Draw connections
+                    const connections = [
+                        [11, 12], [11, 13], [13, 15], [12, 14], [14, 16],
+                        [23, 24], [11, 23], [12, 24], [23, 25], [25, 27],
+                        [27, 29], [29, 31], [24, 26], [26, 28], [28, 30], [30, 32]
+                    ];
+
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                    // Draw the connections
+                    connections.forEach(([start, end]) => {{
+                        const startPoint = landmarks[start];
+                        const endPoint = landmarks[end];
+                        if (startPoint && endPoint) {{
+                            ctx.beginPath();
+                            ctx.moveTo(startPoint.x * canvas.width, startPoint.y * canvas.height);
+                            ctx.lineTo(endPoint.x * canvas.width, endPoint.y * canvas.height);
+                            ctx.strokeStyle = "blue";
+                            ctx.lineWidth = 2;
+                            ctx.stroke();
+                        }}
+                    }});
+
+                    // Draw the landmarks
+                    landmarks.forEach((lm, index) => {{
+                        ctx.beginPath();
+                        ctx.arc(lm.x * canvas.width, lm.y * canvas.height, 3, 0, 2 * Math.PI);
+                        ctx.fillStyle = "red";
+                        ctx.fill();
+                    }});
+                }})();
+            </script>
+        </div>
+        """)
+
+    render_pose.short_description = "Pose Rendering"  # Optional: Display column title
 
 class Routine(models.Model):
     name = models.CharField(max_length=100)
