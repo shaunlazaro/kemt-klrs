791859bea358428a5f12bd2c70723e3f4301f922
shaunlazaro <shaunlazaro@gmail.com>
2025-02-25 13:08:10 -0500

    add exercises page

--- frontend/src/interfaces/exercisePlan.interface.ts
+++ frontend/src/interfaces/exercisePlan.interface.ts
@@ -1,29 +1,10 @@
-// Equivalent to routine config from the python code:
-// class RoutineConfig:
-//     def __init__(self):
-//         self.exercises: Dict[str, Dict[str, Union[ExerciseDetail, float]]] = {}
-
+// Exercise Plan = Routine Config from the python code:
 import { ExerciseDetail, TrackingDetail } from "./exerciseDetail.interface"
 
-//     def add_exercise(
-//         self, 
-//         exercise_name: str, 
-//         exercise_detail: ExerciseDetail, 
-//         reps: Optional[float] = None, 
-//         custom_tracking_details: Optional[List["TrackingDetail"]] = None
-//     ):
-//         self.exercises[exercise_name] = {
-//             "Workout": exercise_detail,
-//             "Reps": reps,
-//             "CustomTrackingDetails": custom_tracking_details or exercise_detail.default_tracking_details,
-//         }
-
-//     def get_workout(self, exercise_name: str):
-//         return self.exercises.get(exercise_name, None)
-
 export interface RoutineConfig{
-    name: string
-    exercises: RoutineComponent[]
+    name: string // TODO: Name is in DB, not in python
+    exercises: RoutineComponent[], // TODO: This is a dict instead of a list incorrectly in python version, fix
+    injury: string // TODO: this is a new addition (not in any other subsystem yet), add everywhere else
 }
 export interface RoutineComponent{
     exercise: ExerciseDetail,
--- frontend/src/pages/patients/patients.tsx
+++ frontend/src/pages/patients/patients.tsx
@@ -3,6 +3,7 @@ import Searchbar from "../../components/searchbar";
 import { DataTable } from "../../components/datatable/data-table";
 import { PatientTableColumnDef } from "../../components/datatable";
 import { MockPatients } from "../../testData/patient";
+import { MdAddCircleOutline } from "react-icons/md";
 
 const Patients: React.FC = () => {
     return (
@@ -11,9 +12,13 @@ const Patients: React.FC = () => {
           Patients
         </div>
         <div className="w-full flex justify-between pb-4">
-          <Searchbar placeholder="Search by name"/>
-          <Button variant="primary">
-            Add Patient
+          <div>
+            <span className="text-primary-darkblue text-sm font-semibold">Search</span><br/>
+            <Searchbar placeholder="Search by name"/>
+          </div>
+          <Button variant="primary" className="h-auto mt-4 mb-1">
+            <MdAddCircleOutline className="h-auto w-6"/>
+            <span className="font-semibold">Add Patient</span>
           </Button>
         </div>
         <DataTable columns={PatientTableColumnDef} data={MockPatients}/>
