319350328df711bbd6ea82af2671cdd13f537844
ckfranz <cfranz@uwaterloo.ca>
2025-02-25 12:37:24 -0500

    tracking updates

--- media-pipe/routines/tracking.py
+++ media-pipe/routines/tracking.py
@@ -16,12 +16,18 @@ class TrackingDetail:
         self,
         tracking_type: TrackingType,
         keypoints: List[str],
+        dimensionality: Optional[str] = "3D",
+        goal_flexion: Optional[float] = None,
+        goal_extension: Optional[float] = None,
         show_alert_if_above: Optional[float] = None,
         show_alert_if_below: Optional[float] = None,
         alert_message: Optional[str] = None
     ):
         self.tracking_type = tracking_type
         self.keypoints = keypoints
+        self.dimensionality = dimensionality
+        self.goal_flexion = goal_flexion  # Store the goal flexion/extension angle
+        self.goal_extension = goal_extension  # Store the goal flexion/extension angle
         self.show_alert_if_above = show_alert_if_above
         self.show_alert_if_below = show_alert_if_below
         self.alert_message = alert_message
