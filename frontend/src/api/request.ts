781ad3f98389d874e336eba214697f062275e277
shaunlazaro <shaunlazaro@gmail.com>
2025-03-18 22:19:44 -0400

    point to prod

--- frontend/src/api/request.ts
+++ frontend/src/api/request.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 // import { useSessionStore } from "./session";
 
-// const apiBase = "http://140.238.151.117:8000/api";
-const apiBase = "http://127.0.0.1:8000/api";
+const apiBase = "http://140.238.151.117:8000/api";
+// const apiBase = "http://127.0.0.1:8000/api";
 
 const axiosInstance = axios.create({
     baseURL: apiBase,
