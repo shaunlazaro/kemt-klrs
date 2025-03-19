a054d63faca641c19a33f002e3777e7e3ac58bb3
jwong2947 <jonathanwong0530@gmail.com>
2025-03-18 23:47:48 -0400

    removed all unneccesary scripts (outdated), added current working bluetooth arduino code

--- pan-tilt-mount/move_motors_esp32.ino
+++ pan-tilt-mount/move_motors_esp32.ino
@@ -34,6 +34,38 @@ int get_number_from_serial(String data, int num_index) {
   return pos.toInt();
 }
 
+String extractPair(String input, int pairIndex) {
+  int startPos = 0;
+  int pairCount = 0;
+  
+  // Loop to find the pair at the specified index
+  while (pairCount < pairIndex) {
+    startPos = input.indexOf('[', startPos);  // Find the start bracket
+    if (startPos == -1) {
+      return "";  // If no more pairs are found, return an empty string
+    }
+    
+    int endPos = input.indexOf(']', startPos);  // Find the end bracket
+    if (endPos == -1) {
+      return "";  // If no closing bracket is found, return an empty string
+    }
+    
+    // Move start position past the closing bracket for the next iteration
+    startPos = endPos + 1;  
+    pairCount++;  // Increment the pair count
+  }
+
+  // Now extract the pair at the given index
+  int startPosBracket = input.indexOf('[', startPos) + 1;  // Move after the opening bracket
+  int endPosBracket = input.indexOf(']', startPosBracket);  // Find the closing bracket
+
+  if (startPosBracket != -1 && endPosBracket != -1) {
+    return input.substring(startPosBracket, endPosBracket);  // Return the pair without brackets
+  }
+  return "";  // In case something went wrong
+}
+
+
 void setup() {
   Serial.begin(115200); // Debugging on Serial Monitor
   // bluetooth setup
@@ -45,6 +77,7 @@ void setup() {
   vServo.attach(verticalServoPin);
   vServo.write(45); // default position when start
   hServo.write(90);
+
   vposcurrent = 45;
   hposcurrent = 90;
 }
@@ -53,19 +86,47 @@ void loop() {
   if (SerialBT.hasClient() && !isConnected) {
     Serial.println("Bluetooth Device Connected!");
     isConnected = true;
+
   }
   if (!SerialBT.hasClient() && isConnected) {
     Serial.println("Bluetooth Device Disconnected!");
     isConnected = false;
+
+    // if client connected, then disconnect, set back to starting pos
+    vpostarget = 45;
+    hpostarget = 90;
+    Serial.println("Reseting to default position");
+    // flush all previous client dats
+    SerialBT.flush();
+    Serial.println("flushed all previous buffers and client data");
   }
 
   if (SerialBT.available()) {
     String data = SerialBT.readString(); // Read data from Bluetooth
     int count = std::count(data.begin(), data.end(), targetchar);
-    if (count == 1) {
-      vpostarget = get_number_from_serial(data, 0);
-      hpostarget = get_number_from_serial(data, 1);  
+    Serial.println(count);
+    if (count > 0) {
+      String parsed_data = extractPair(data, count-1);
+      Serial.println(parsed_data);
+      vpostarget = get_number_from_serial(parsed_data, 0);
+      hpostarget = get_number_from_serial(parsed_data, 1);  
+
     }
+
+    // TEMP hard limits for first prototype
+    if (hpostarget >= 150) {
+      hpostarget = 150;
+    }
+    if (hpostarget <= 30) {
+      hpostarget=30;
+    }
+    if (vpostarget >= 120) {
+      vpostarget = 120;
+    }
+    if (vpostarget <= 20) {
+      vpostarget=20;
+    }
+
     Serial.println("Received: " + data); // print to serial for checking if recieved 
     SerialBT.println("Echo: " + data); // Echo the data back
   }
