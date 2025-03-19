b7a3e5d79a5fc36489cc1d5289aee36cb3503150
Allen Liu <allen@Allens-MacBook-Pro.local>
2025-03-19 17:47:09 -0400

    update text on workout screens

--- PhysiKneed App/app/src/main/java/com/example/physiokneeds_v3/MainActivity.kt
+++ PhysiKneed App/app/src/main/java/com/example/physiokneeds_v3/MainActivity.kt
@@ -541,9 +541,9 @@ class MainActivity : AppCompatActivity() {
 //                    leftImage.setImageResource(R.drawable.positioned)
                     videoFeed.visibility = View.VISIBLE
                     // update text
-                    titleText.text = "Get Positioned"
+                    titleText.text = "Get Positioned in Frame"
                     leftText.visibility = View.VISIBLE
-//                    leftText.text = "Stand 2 m away from camera \n\n Your entire body should be visible on the screen \n\n Your first exercise will start automatically once tracking is completed"
+//                    leftText.text = "Stand 2 m away from the camera. \n\n Exercises start automatically once your entire body is visible on screen."
                     // TODO more polish for UI
                     // TODO update image with GIF
 
@@ -577,7 +577,7 @@ class MainActivity : AppCompatActivity() {
 //                    tipText.visibility = View.VISIBLE
 //                    tipText.text = "TIPS"
                     titleText.text = routineConfig.exercises[currentExerciseIndex].exercise.displayName
-                    leftText.text = "TIPS \n\n1. Sit facing sideways to the camera.\n\n2. Raise and lower your leg slowly"
+                    leftText.text = "TIPS \n\n1. Sit facing sideways to the camera.\n\n2. Raise and lower your leg slowly."
                     if (routineConfig.exercises[currentExerciseIndex].exercise.displayName == "Seated Leg Extension (Left)") {
                         val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.seated_leg_extension)
                         videoFeed.setVideoURI(videoUri)
